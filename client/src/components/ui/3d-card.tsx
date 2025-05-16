d748a5555b81d1be7db967ec3c76ced2f7bcbea9
Azizaman <azizamanaaa97@gmail.com>
2025-05-16 16:37:22 +0530

    all working except chat 6

--- client/src/components/ui/3d-card.tsx
+++ client/src/components/ui/3d-card.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import { cn } from "@/lib/utils";
-import Image from "next/image";
+
 import React, {
   createContext,
   useState,
